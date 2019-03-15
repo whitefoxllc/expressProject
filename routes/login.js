f1796ac66956062906fc9041846cff4397130da3
Alex Dunn <alexdunnmobile@gmail.com>
2019-03-14 20:32:11 -0700

    Added Bootstrap and some minor styling to existing pages.

--- routes/login.js
+++ routes/login.js
@@ -5,17 +5,13 @@ var login = require("../login-helper.js");
 
 /* GET login attempt. */
 router.post('/', function(req, res, next) {
-    console.log(`attempted login with username=${req.body.username} and hash=${req.body.password}`);
     login.passwordIsValid(req.body.username, req.body.password,function (valid) {
         if (valid) {
-            console.log("Login Successful");
             req.session.user = req.body.username;
-            console.log(`set session.user=${req.session.user}`);
             res.render('userlandingpage', { title: 'success', user: req.session.user});
         }
         else {
-            console.log("Login Unsuccessful");
-            res.render('login', { title: 'fail' });
+            res.render('loginfailed', { title: 'Log in to Whitefox' });
         }
     });
 });
@@ -24,9 +20,8 @@ router.post('/', function(req, res, next) {
 /* GET logout*/
 router.get('/logout', function(req, res, next) {
     req.session.destroy(function(){
-        console.log("user logged out.")
+        res.redirect('/');
     });
-    res.redirect('/');
 });
 
 module.exports = router;
