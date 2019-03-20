4e6d45ea772e895dd0542fac5d023e9c8e80d2ff
A. Dunn <alexdunnmobile@gmail.com>
2019-03-20 12:09:45 -0700

    Made the login routes consistent between login and recognition of session token (can be renamed later) and migrated functionality to login.js route

--- routes/login.js
+++ routes/login.js
@@ -5,15 +5,23 @@ var login = require("../login-helper.js");
 
 /* GET login attempt. */
 router.post('/', function(req, res, next) {
-    login.passwordIsValid(req.body.username, req.body.password, function (valid) {
-        if (valid) {
-            req.session.user = req.body.username;
-            //res.render('userlandingpage', { title: 'success', user: req.session.user});
-        }
-        else {
-            res.render('loginfailed', { title: 'Log in to Whitefox' });
-        }
-    });
+
+    if (req.session.user) {
+        //render user landing page
+        res.render('userlandingpage', { title: 'Whitefox Streaming Video', message: `Welcome, ${req.session.user}!`, user: req.session.user });
+    }
+    else {
+        login.passwordIsValid(req.body.username, req.body.password, function (valid) {
+            if (valid) {
+                req.session.user = req.body.username;
+                res.render('userlandingpage', {title: 'Whitefox Streaming Video', message: `Welcome, ${req.session.user}!`, user: req.session.user });
+            }
+            else {
+                res.render('loginfailed', { title: 'Log in to Whitefox' });
+            }
+        });
+    }
+
 });
 
 /* GET logout*/
