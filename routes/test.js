732a8291ceb152a9ec7522b7306f238e78e195aa
Alex Dunn <alexdunnmobile@gmail.com>
2019-03-22 15:11:09 -0700

    Tested subscription helper functions against session store only.

--- routes/test.js
+++ routes/test.js
@@ -10,7 +10,39 @@ router.get('/', function(req, res, next) {
 
     if (req.session.user) {
         db.readOnlyConnection.query(`SELECT id, title FROM productions;`, function(err, rows, fields) {
-            sub.activateSubscription(req);
+            console.log(`Subscription should be null: ${req.session.subscriptionActiveUntil}`);
+            console.log(`Subscription ${sub.subscriptionActive(req) ? "is" : "is not"} active.\n`);
+            sub.activateSubscription(req, 1);
+            console.log(`Subscription should be a month from now: ${req.session.subscriptionActiveUntil}`);
+            console.log(`Subscription ${sub.subscriptionActive(req) ? "is" : "is not"} active.\n`);
+
+            sub.renewSubscription(req);
+            console.log(`Subscription should be two months from now: ${req.session.subscriptionActiveUntil}`);
+            console.log(`Subscription ${sub.subscriptionActive(req) ? "is" : "is not"} active.\n`);
+
+            // sub.cancelSubscription(req);
+            // console.log(`Subscription should be null: ${req.session.subscriptionActiveUntil}`);
+            // console.log(`Subscription ${sub.subscriptionActive(req) ? "is" : "is not"} active.\n`);
+
+            var myProd = "theOffice";
+            var myProd2 = "mrRobot";
+
+            console.log(`User ${sub.hasAccessTo(req, myProd) ? "has" : "has no"} access to ${myProd}.`);
+            console.log(`Requesting access to ${myProd} and ${myProd2}.`);
+            console.log(`User is ${sub.requestAccessTo(req, myProd) ? "granted" : "not granted"} access to ${myProd}.`);
+            console.log(`User is ${sub.requestAccessTo(req, myProd2) ? "granted" : "not granted"} access to ${myProd2}.`);
+            console.log(`User ${sub.hasAccessTo(req, myProd) ? "has" : "has no"} access to ${myProd}.`);
+            console.log(`User ${sub.hasAccessTo(req, myProd2) ? "has" : "has no"} access to ${myProd2}.\n`);
+
+            console.log(`Removing access to ${myProd}.`);
+            sub.removeAccessTo(req, myProd);
+            console.log(`User ${sub.hasAccessTo(req, myProd) ? "has" : "has no"} access to ${myProd}.\n`);
+            console.log(`Requesting access to ${myProd2} again.`);
+            console.log(`User is ${sub.requestAccessTo(req, myProd2) ? "granted" : "not granted"} access to ${myProd2}.`);
+            console.log(`User ${sub.hasAccessTo(req, myProd) ? "has" : "has no"} access to ${myProd}.`);
+            console.log(`User ${sub.hasAccessTo(req, myProd2) ? "has" : "has no"} access to ${myProd2}.`);
+
+
             res.render('test', { title: 'Whitefox Streaming Video', message: `Welcome, ${req.session.user}!`, user: req.session.user, displayProductions: rows});
         });
 
