7c2c2d6fa92852c397dc9e640f2e763b52b53491
Alex Dunn <alexdunnmobile@gmail.com>
2019-03-23 12:05:34 -0700

    Db synchronisation performed upon request to /home.  Tested

--- routes/home.js
+++ routes/home.js
@@ -2,20 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var db = require("../db-helper");
+var sub = require("../subscription-helper");
+
 
 router.get('/', function(req, res, next) {
 
     if (req.session.user) {
-        //at present, the home page displays all productions.  In future, feature-specific functionality will determine displayProductions
-        db.readOnlyConnection.query(`SELECT id, title FROM productions;`, function(err, rows, fields) {
-            res.render('home', {
-                title: 'Whitefox Streaming Video',
-                message: `Welcome, ${req.session.user}!`,
-                user: req.session.user,
-                displayProductions: rows
+        sub.syncSessionWithDb(req, function () {
+            //at present, the home page displays all productions.  In future, feature-specific functionality will determine displayProductions
+            db.readOnlyConnection.query(`SELECT id, title FROM productions;`, function(err, rows, fields) {
+                console.log(req.session);
+                res.render('home', {
+                    title: 'Whitefox Streaming Video',
+                    message: `Welcome, ${req.session.user}!`,
+                    user: req.session.user,
+                    displayProductions: rows
+                });
             });
         });
-
     }
     else {
         res.redirect("/");
