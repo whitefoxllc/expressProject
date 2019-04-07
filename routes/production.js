e04324ca643983d208ef911705b56a3f1acd47d5
rjbena <45724673+rjbena@users.noreply.github.com>
2019-04-07 15:20:50 -0700

    Update production.js
    Added search helper which allows the query of production title and id.

--- routes/production.js
+++ routes/production.js
@@ -2,43 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 var db = require("../db-helper");
+var search = require('../search-helper');
+router.get('/', function(req, res, next) {
+    search.getIdTitle(req,function (sBar) {
+        if (req.session.user) {
+            //fetch data production data from db
+            db.readOnlyConnection.query(`SELECT * FROM productions where id = "${req.query.production}";`, function (err, rows, fields) {
+                var productionData = rows;
 
-    router.get('/', function(req, res, next) {
-    if (req.session.user) {
-        //fetch data production data from db
-        db.readOnlyConnection.query(`SELECT * FROM productions where id = "${req.query.production}";`, function(err, rows, fields) {
-            var productionData = rows;
+                //if a season has been selected, fetch episodes' data, else render the page without it
+                if (req.query.seasonSelection) {
+                    db.readOnlyConnection.query(`SELECT episodeNumber, title, fileURL FROM episodes WHERE production = "${req.query.production}" AND seasonNumber = "${req.query.seasonSelection}";`, function (err, rows, fields) {
 
-            //if a season has been selected, fetch episodes' data, else render the page without it
-            if (req.query.seasonSelection) {
-                db.readOnlyConnection.query(`SELECT episodeNumber, title, fileURL FROM episodes WHERE production = "${req.query.production}" AND seasonNumber = "${req.query.seasonSelection}";`, function(err, rows, fields) {
-
+                        res.render('production', {
+                            title: 'Whitefox Streaming Video',
+                            user: req.session.user,
+                            productionData: productionData[0],
+                            season: req.query.seasonSelection,
+                            episodeCount: rows.length,
+                            episodeData: rows,
+                            production_list: sBar
+                        });
+                    });
+                } else {
+                    console.log("No season selected");
                     res.render('production', {
                         title: 'Whitefox Streaming Video',
                         user: req.session.user,
                         productionData: productionData[0],
-                        season: req.query.seasonSelection,
-                        episodeCount: rows.length,
-                        episodeData: rows
+                        season: null,
+                        episodeData: null,
+                        production_list: sBar
                     });
-                });
-            }
-            else {
-                console.log("No season selected");
-                res.render('production', {
-                    title: 'Whitefox Streaming Video',
-                    user: req.session.user,
-                    productionData: productionData[0],
-                    season: null,
-                    episodeData: null
-                });
-            }
-        });
+                }
+            });
+        } else {
+            res.redirect("/");
+        }
     }
-    else {
-        res.redirect("/");
-    }
-
 });
 
 module.exports = router;
