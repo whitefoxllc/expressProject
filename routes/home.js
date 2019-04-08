e9cb56a57f161fafa25bbbd1f4b364d754c43538
rjbena <45724673+rjbena@users.noreply.github.com>
2019-04-08 07:10:33 -0700

    Update home.js
    added search helper to have the navigation search bar work.

--- routes/home.js
+++ routes/home.js
@@ -6,27 +6,26 @@ var sub = require("../subscription-helper");
 var search = require("../search-helper");
 
 router.get('/', function(req, res, next) {
-    search.getIdTitle(req,function (sBar) {
+    search.getIdTitle(req,function (find) {
+
         if (req.session.user) {
             sub.syncSessionWithDb(req, function () {
                 //at present, the home page displays all productions.  In future, feature-specific functionality will determine displayProductions
                 db.readOnlyConnection.query(`SELECT id, title FROM productions;`, function (err, rows, fields) {
-
                     console.log(req.session);
                     res.render('home', {
                         title: 'Whitefox Streaming Video',
                         message: `Welcome, ${req.session.user}!`,
                         user: req.session.user,
                         displayProductions: rows,
-                        genre: req.query.genreSelection,
-                        production_list: sBar
+                        production_list:find
                     });
                 });
             });
         } else {
             res.redirect("/");
         }
-    }
+    });
 });
 
 module.exports = router;
