69ddaefd3e886c5b0d9b259347ebad76b0de4348
A. Dunn <alexdunnmobile@gmail.com>
2019-04-10 10:55:09 -0700

    replaced navbar-related .js file

--- routes/production.js
+++ routes/production.js
@@ -6,25 +6,28 @@ var search = require("../search-helper");
 
 router.get('/', function(req, res, next) {
     search.getIdTitle(req,function (find) {
-        if (req.session.user) {
-            db.readOnlyConnection.query(`SELECT * FROM productions where id = "${req.query.production}";`, function (err, rows, fields) {
-                var productionData = rows;
-                var season = req.query.seasonSelection ? req.query.seasonSelection : 1;
-                db.readOnlyConnection.query(`SELECT episodeNumber, title, fileURL FROM episodes WHERE production = "${req.query.production}" AND seasonNumber = "${season}";`, function (err, rows, fields) {
-                    res.render('production', {
-                        title: 'Whitefox Streaming Video',
-                        user: req.session.user,
-                        productionData: productionData[0],
-                        season: season,
-                        episodeCount: rows.length,
-                        episodeData: rows,
-                        production_list: find
+        search.getGenre(req, function (findGenre) {
+            if (req.session.user) {
+                db.readOnlyConnection.query(`SELECT * FROM productions where id = "${req.query.production}";`, function (err, rows, fields) {
+                    var productionData = rows;
+                    var season = req.query.seasonSelection ? req.query.seasonSelection : 1;
+                    db.readOnlyConnection.query(`SELECT episodeNumber, title, fileURL FROM episodes WHERE production = "${req.query.production}" AND seasonNumber = "${season}";`, function (err, rows, fields) {
+                        res.render('production', {
+                            title: 'Whitefox Streaming Video',
+                            user: req.session.user,
+                            productionData: productionData[0],
+                            season: season,
+                            episodeCount: rows.length,
+                            episodeData: rows,
+                            production_list: find,
+                            genre_list: findGenre
+                        });
                     });
                 });
-            });
-        } else {
-            res.redirect("/");
-        }
+            } else {
+                res.redirect("/");
+            }
+        });
     });
 });
 
