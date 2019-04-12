1d316c187e57a84e2003a927adfd258a24d3ea8e
anthonyR <30310290+anthonyrodriguez0994@users.noreply.github.com>
2019-04-12 13:42:35 -0700

    added userProfile.js

---
+++ routes/userProfile.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require("../db-helper");
+var sub = require("../subscription-helper");
+var search = require("../search-helper");
+
+router.get('/', function(req, res, next) {
+
+
+});
+
+module.exports = router;
