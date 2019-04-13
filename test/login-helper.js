62183755a564910b7562093b13e1a61128feb4ac
Alex Dunn <alexdunnmobile@gmail.com>
2019-04-12 21:52:26 -0700

    coverage testing is now fully operational

--- test/login-helper.js
+++ test/login-helper.js
@@ -1,7 +1,23 @@
 const assert  = require('assert');
 
 //login-helper tests
+db = require("../db-helper");
 login = require("../login-helper");
+
+//this needs to end up as a connection pool so it can be closed in one go, but I'll do that later
+after(function (done) {
+    db.readOnlyConnection.end(function () {
+        db.sessionStoreConnection.end(function () {
+            db.writeProdsConnection.end(function () {
+                db.writeSubscriptionsConnection.end(function () {
+                    db.writeUsersConnection.end(function () {
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
 
 describe("login-helper", function () {
     describe("hash", function () {
