557df3299654439b3f98fdd1d7f4c4cc374b7559
anthonyR <30310290+anthonyrodriguez0994@users.noreply.github.com>
2019-04-19 10:15:53 -0700

    userRouter fix

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/user', user);
+app.use('/user', userRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
 app.use('/register', registerRouter);
