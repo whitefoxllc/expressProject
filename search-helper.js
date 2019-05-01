17f47adf9ff8fe40e3d0b1443f1d66ecf9b5a757
Alex Dunn <alexdunnmobile@gmail.com>
2019-04-30 20:54:08 -0700

    refactored search-helper

--- search-helper.js
+++ search-helper.js
@@ -1,35 +1,22 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'whiteFoxReadOnly',
-    password: 'wfro5463!',
-    database: 'whitefoxdb'
-});
+var db = require("./db-helper");
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log('You are now connected to database');
-});
+
 
 //Gets production title and ID
 var getAllIdsTitles = function (req, callback) {
-    connection.query('SELECT title , id FROM productions ',
+    db.readOnlyConnection.query('SELECT title , id FROM productions ',
         function (err, rows, fields) {
             if (err) throw (err);
-
             return callback(rows);
-
-        });
+    });
 };
 
 var getAllGenres = function (req, callback) {
-    connection.query('SELECT genre FROM genres ',
+    db.readOnlyConnection.query('SELECT genre FROM genres ',
         function (err, genres, fields) {
             if (err) throw (err);
-
             return callback(genres);
-
-        });
+    });
 };
 exports = module.exports ={getAllIdsTitles,getAllGenres};
 
