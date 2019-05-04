03be077bcc6cf4a70b8d4a88cd659537dae4f61a
Alex Dunn <alexdunnmobile@gmail.com>
2019-05-03 18:31:21 -0700

    Numerous date interaction fixes - dates are pulled from sql as Date objects and stripped of their erroneous gmt offsets

--- date-helper.js
+++ date-helper.js
@@ -8,8 +8,24 @@ function dateToSqlDatetime(date) {
 }
 
 function dateFromSqlDatetime(datetime) {
-    let date = new Date(datetime + " UTC");
+    let date = new Date();
+    date.setUTCFullYear(parseInt(datetime.substring(0, 4)));
+    date.setUTCMonth(parseInt(datetime.substring(5, 7)));
+    date.setUTCDate(parseInt(datetime.substring(8, 10)));
+    date.setUTCHours(parseInt(datetime.substring(11, 13)));
+    date.setUTCMinutes(parseInt(datetime.substring(14, 16)));
+    date.setUTCSeconds(parseInt(datetime.substring(17, 18)));
     return date;
+}
+
+//strips the gmt offset from a UTC-format sql datetime which has been erroneously assigned an offset
+function stripGmtOffset(date) {
+    let correctDate = new Date();
+    correctDate.setUTCFullYear(date.getFullYear());
+    correctDate.setUTCMonth(date.getMonth());
+    correctDate.setUTCDate(date.getDate());
+    correctDate.setUTCHours(date.getHours(), date.getMinutes(), date.getSeconds());
+    return correctDate;
 }
 
 function timesAreApproximatelyEqual(t1, t2) {
@@ -17,4 +33,4 @@ function timesAreApproximatelyEqual(t1, t2) {
     return  differenceInMilliseconds < 10000;
 }
 
-exports = module.exports = {dateToSqlDatetime, dateFromSqlDatetime, timesAreApproximatelyEqual};
+exports = module.exports = {dateToSqlDatetime, dateFromSqlDatetime, stripGmtOffset, timesAreApproximatelyEqual};
--- test/updateUser-helper.js
+++ test/updateUser-helper.js
@@ -4,15 +4,18 @@ db = require("../db-helper");
 user = require("../updateUser-helper");
 
 before(function (done) {
-    db.coverageRootConnection.query(`insert into users(username, emailaddress, dateOfBirth) 
-                                    values('updateUserTest-1', 'revTest@rev.test', '2000-01-01 00:00:00')`, function (err, rows, fields) {
+    db.coverageRootConnection.query(`delete from users where username like 'updateUserTest-%'`, function (err, rows, fields) {
         if (err) throw err;
         db.coverageRootConnection.query(`insert into users(username, emailaddress, dateOfBirth) 
+                                    values('updateUserTest-1', 'revTest@rev.test', '2000-01-01 00:00:00')`, function (err, rows, fields) {
+            if (err) throw err;
+            db.coverageRootConnection.query(`insert into users(username, emailaddress, dateOfBirth) 
                                     values('updateUserTest-2', 'revTest@rev.test', '2000-01-01 00:00:00')`, function (err, rows, fields) {
-            db.coverageRootConnection.query(`delete from users where username='updateUserTest-3'`, function (err, rows, fields) {
-                db.coverageRootConnection.query(`insert into paymentcredentials values('updateUserTest-1','test','test','1234123412341234',1,2000)`, function (err, rows, fields) {
-                    db.coverageRootConnection.query(`insert into userbillingaddresses(user) values('updateUserTest-1')`, function (err, rows, fields) {
-                        done();
+                db.coverageRootConnection.query(`delete from users where username='updateUserTest-3'`, function (err, rows, fields) {
+                    db.coverageRootConnection.query(`insert into paymentcredentials values('updateUserTest-1','test','test','1234123412341234',1,2000)`, function (err, rows, fields) {
+                        db.coverageRootConnection.query(`insert into userbillingaddresses(user) values('updateUserTest-1')`, function (err, rows, fields) {
+                            done();
+                        });
                     });
                 });
             });
